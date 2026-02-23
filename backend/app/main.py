ffa64aab1397fe4795c826d3d1289eac79d749c7
Harshitha <kovvuriharshitha96@gmail.com>
2026-02-23 19:16:26 +0530

    added frontend vercel link in backend folder

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://127.0.0.1:5174",
         "https://business-sales-and-profit-analyzer.onrender.com/",
-        "https://business-sales-and-profit-analyzer.onrender.com",
+        "https://business-sales-profit-analyzer.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
