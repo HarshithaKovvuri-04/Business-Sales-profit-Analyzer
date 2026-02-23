38b5c24e69686124e89f14b82af73bf2d6b4675f
Harshitha <kovvuriharshitha96@gmail.com>
2026-02-23 18:45:14 +0530

    added backend render url in backend folder

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,6 +26,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
+        "https://business-sales-and-profit-analyzer.onrender.com/",
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
